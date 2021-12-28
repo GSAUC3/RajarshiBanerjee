34631144d689e744a527fe9ea38e9b6a8652f9ea
Rajarshi Banerjee <rajarshiban13@gmail.com>
2021-12-28 14:53:36 +0530

    done

--- js/app.js
+++ js/app.js
@@ -14,3 +14,18 @@ function toggleButton() {
 }
 
 
+// // let bg = document.getElementById("bg")
+// let moon = document.getElementById("moon")
+// let mountain = document.getElementById("mountain")
+// let road = document.getElementById("road")
+
+// window.addEventListener('scroll',function(){
+//     var value = window.scrollY;
+
+//     // bg.style.top= value*0.5+'px';
+//     // moon.style.transform='translateY('+ value*(-0.5)+'px)';
+//     moon.style.left= -value*0.5+'px';
+//     mountain.style.top= -value*0.1+'px';
+//     road.style.top= value*0.3+'px';
+
+// })
